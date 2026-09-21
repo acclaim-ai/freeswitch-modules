71980af6551b936bc86203674ae6e8e2e4dc1cd8
igorlichenko-aif <iurii.gorlichenko@acclaim.ai>
2026-09-21 23:02:57 +0200

    init playout with NULL

--- mod_audio_fork/lws_glue.cpp
+++ mod_audio_fork/lws_glue.cpp
@@ -933,7 +933,7 @@ extern "C" {
 
     if (!tech_pvt) return SWITCH_STATUS_FALSE;
     drachtio::AudioPipe *pAudioPipe = static_cast<drachtio::AudioPipe *>(tech_pvt->pAudioPipe);
-    struct playout* = NULL;
+    struct playout* playout = NULL;
 
     switch_mutex_lock(tech_pvt->mutex);
 
